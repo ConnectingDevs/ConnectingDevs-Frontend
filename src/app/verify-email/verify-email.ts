07963cc4e16f92f64bb90f865b698bc0cb16ed9d
CarlosBastianLérida <carlosbastialerida@gmail.com>
2026-05-31 18:28:15 +0200

    fix: quitar standalone de VerifyEmail

--- src/app/verify-email/verify-email.ts
+++ src/app/verify-email/verify-email.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-verify-email',
